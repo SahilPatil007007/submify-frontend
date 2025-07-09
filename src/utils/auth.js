102eb18920503f47fdd136f37156a3fbe754d65e
Sahil Patil <sahilpatil221004@gmail.com>
2025-07-09 14:52:31 +0530

    Remove print statements

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,4 @@
 export const getAuth = () => {
-    console.log(localStorage.getItem('token'));
     return {
         token: localStorage.getItem('token'),
         user: JSON.parse(localStorage.getItem('user'))
