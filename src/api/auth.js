1ec60073758e37ec1ed9837845579646bf74f567
Sahil Patil <sahilpatil221004@gmail.com>
2025-07-09 14:26:42 +0530

    Backend URL Change

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const signup = (data) => axios.post(`${BASE_URL}/auth/signup`, data);
 export const login = (data) => axios.post(`${BASE_URL}/auth/login`, data);
--- src/api/teacher.js
+++ src/api/teacher.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getAuth } from '../utils/auth';
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 const getAuthHeaders = () => ({
   headers: {
